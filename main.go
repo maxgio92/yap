3c1ab5f46402a28345384de5ac1bff9d2844d243
Massimiliano Giovagnoli <me@maxgio.it>
2024-04-12 16:48:51 +0200

    refactor(main.go): validate flag values
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/maxgio92/perf-profiler-go/pkg/profile"
 )
@@ -11,16 +12,32 @@ import (
 func main() {
 	var pid, duration int
 	flag.IntVar(&pid, "pid", 0, "The PID of the process")
-	flag.IntVar(&duration, "duration", 0, "The duration in seconds for the profiling")
+	flag.IntVar(&duration, "duration", 30, "The duration in seconds for the profiling")
+
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [options] [command]\n", path.Base(os.Args[0]))
+		fmt.Printf("Options:\n")
+		flag.PrintDefaults()
+	}
 
 	flag.Parse()
+
+	if pid == 0 {
+		fmt.Println("pid is mandatory")
+		os.Exit(1)
+	}
+
+	if duration <= 0 {
+		fmt.Println("duration must be greater than 0")
+		os.Exit(1)
+	}
 
 	profiler := profile.NewProfile(
 		profile.WithPID(pid),
 		profile.WithDuration(duration),
 		profile.WithSamplingPeriodMillis(11),
 		profile.WithProbeName("sample_stack_trace"),
-		profile.WithProbeFilepath("kernel/profile.o"),
+		profile.WithProbeFilepath("profile.o"),
 		profile.WithMapStackTraces("stack_traces"),
 		profile.WithMapHistogram("histogram"),
 	)
