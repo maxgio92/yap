687799f5bcc2bf4d5e57f8e71f3ed643ea45db7a
Massimiliano Giovagnoli <me@maxgio.it>
2024-11-29 20:11:59 +0100

    refactor(symtable): introduce cache from symcache
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- pkg/symtable/symtable.go
+++ pkg/symtable/symtable.go
@@ -2,38 +2,47 @@ package symtable
 
 import (
 	"debug/elf"
+	"github.com/maxgio92/yap/pkg/symcache"
 	"github.com/pkg/errors"
 )
 
 var (
-	ErrSymtableEmpty = errors.New("symtable is empty")
+	ErrSymTableEmpty = errors.New("symtable is empty")
 )
 
 // ELFSymTab is one of the possible abstractions around executable
 // file symbol tables, for ELF files.
 type ELFSymTab struct {
 	symtab []elf.Symbol
+	cache  *symcache.SymCache
 }
 
 func NewELFSymTab() *ELFSymTab {
-	return new(ELFSymTab)
+	tab := new(ELFSymTab)
+	tab.symtab = make([]elf.Symbol, 0)
+	tab.cache = symcache.NewSymCache()
+
+	return tab
 }
 
 // Load loads from the underlying filesystem the ELF file
 // with debug/elf.Open and stores it in the ELFSymTab struct.
 func (e *ELFSymTab) Load(pathname string) error {
 	// Skip load if file elf.File has already been loaded.
-	if e.symtab != nil {
+	if e.symtab != nil && len(e.symtab) > 0 {
 		return nil
 	}
+
 	file, err := elf.Open(pathname)
 	if err != nil {
 		return errors.Wrap(err, "error opening ELF file")
 	}
+
 	syms, err := file.Symbols()
 	if err != nil {
 		return errors.Wrap(err, "error reading ELF symtable section")
 	}
+
 	e.symtab = syms
 
 	return nil
@@ -42,13 +51,20 @@ func (e *ELFSymTab) Load(pathname string) error {
 // GetSymbol returns symbol name from an instruction pointer address
 // reading the ELF symbol table.
 func (e *ELFSymTab) GetSymbol(ip uint64) (string, error) {
-	var sym string
-	if e.symtab == nil {
-		return "", ErrSymtableEmpty
-	}
-	for _, s := range e.symtab {
-		if ip >= s.Value && ip < (s.Value+s.Size) {
-			sym = s.Name
+	// Try from cache.
+	sym, err := e.cache.Get(ip)
+	if err != nil {
+		// Cache miss.
+		if e.symtab == nil || len(e.symtab) == 0 {
+			return "", ErrSymTableEmpty
+		}
+		for _, s := range e.symtab {
+			if ip >= s.Value && ip < (s.Value+s.Size) {
+				sym = s.Name
+			}
+		}
+		if e.cache != nil {
+			e.cache.Set(sym, ip)
 		}
 	}
 
